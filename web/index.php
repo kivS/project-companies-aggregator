d691da6bb2a2e3830d139b71091f8b1cdd755c2d
Vik Borges <v1k@protonmail.com>
2021-12-28 23:33:30 +0000

    rough user feedback

--- web/index.php
+++ web/index.php
@@ -3,8 +3,41 @@ require_once(__DIR__ . '/../.env.php');
 require_once __DIR__ . '/vendor/autoload.php';
 
 use MeiliSearch\Client;
+use GuzzleHttp\Client as GuzzleClient;
 
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] == '/send-feedback') {
+    header('Content-Type: application/json');
+
+    $email = $_POST['email'];
+    $text = $_POST['text'];
+
+    if(!$email || !$text) {
+        echo json_encode(['error' => 'email and text are required']);
+        exit();
+    }
+
+    // send http request
+    $client = new GuzzleClient();
+
+    $message = "
+User feedback for [companies aggregator project]
+Email: {$email}
+
+{$text}
+    ";
+
+    $response = $client->request('POST', 'https://api.telegram.org/bot'.TELEGRAM_BOT_TOKEN.'/sendMessage', [
+        'form_params' => [
+            'chat_id' => TELEGRAM_MY_CHAT_ID,
+            'text' => $message,
+        ]
+    ]);
+
+    
+    echo json_encode(['success' => true]);
+    exit();
+}
 
 if (isset($_GET['problem'])) {
     try {
@@ -53,7 +86,7 @@ if (isset($_GET['problem'])) {
             text-align: center;
         }
 
-        li{
+        li {
             border: 1px solid red;
         }
     </style>
@@ -63,7 +96,7 @@ if (isset($_GET['problem'])) {
     <h1>Discover what companies are working on what problems</h1>
 
     <form action="" method="get">
-        <input type="search" list="problems-completion" name="problem" autocomplete="off" placeholder="water, energy, climate change, etc...">
+        <input type="search" minlength="2" name="problem" autocomplete="off" placeholder="water, energy, climate change, etc...">
     </form>
 
     <?php if (isset($_GET['problem'])) { ?>
@@ -76,7 +109,7 @@ if (isset($_GET['problem'])) {
 
         <?php if ($search_results['nbHits'] > 0) { ?>
             <div>
-                <p>Found <?= $search_results['nbHits']; ?></p>
+                <p><?= $search_results['nbHits']; ?> matches</p>
                 <ul>
                     <?php foreach ($search_results['hits'] as $result) { ?>
                         <li>
@@ -92,13 +125,35 @@ if (isset($_GET['problem'])) {
                     <?php }; ?>
                 </ul>
             </div>
-        <?php }else{ ?>
+        <?php } else { ?>
             <div>
                 <p>No results found</p>
             </div>
         <?php }; ?>
 
     <?php }; ?>
+
+    <!-- form for feedback -->
+    <div>
+        <p>Feedback</p>
+        <form id="feedback" action="" method="POST">
+            <input type="email" name="email" placeholder="your@email.com" required>
+            <textarea name="text" cols="30" rows="10" required></textarea>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+    <script>
+        document.querySelector('form#feedback').addEventListener('submit', async function(e) {
+            e.preventDefault();
+            // send request
+            let request = await fetch('/send-feedback', {
+                method: 'POST',
+                body: new FormData(this)
+            });
+
+            console.log(request);
+        })
+    </script>
 </body>
 
 </html>

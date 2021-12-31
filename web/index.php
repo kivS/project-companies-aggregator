f53421700a23537c3620accb61beaa1b28b8d01d
Vik Borges <v1k@protonmail.com>
2021-12-31 19:40:41 +0000

    search min len of 3

--- web/index.php
+++ web/index.php
@@ -86,7 +86,7 @@ if (isset($_GET['problem']) &&  strlen($_GET['problem']) > 2) {
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                         </svg>
                     </div>
-                    <input type="search" minlength="2" name="problem" autocomplete="off" placeholder="water, energy, climate change, etc..." class="rounded-xl w-full text-center block pl-10">
+                    <input type="search" minlength="3" name="problem" autocomplete="off" placeholder="water, energy, climate change, etc..." class="rounded-xl w-full text-center block pl-10">
                 </div>
             </form>
 

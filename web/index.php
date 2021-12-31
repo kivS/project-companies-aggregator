40dcf13a8c3c77e1484233bdd3ac02ec2c581d09
Vik Borges <v1k@protonmail.com>
2021-12-31 23:23:32 +0000

    make search required so we don't have empty strings

--- web/index.php
+++ web/index.php
@@ -91,7 +91,7 @@ if (isset($_GET['problem']) &&  strlen($_GET['problem']) > 1) {
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                         </svg>
                     </div>
-                    <input type="search" tabindex="0" minlength="2" name="problem" autocomplete="off" placeholder="water, energy, climate change, etc..." class="rounded-xl w-full text-center block pl-10">
+                    <input type="search" tabindex="0" minlength="2" name="problem" required autocomplete="off" placeholder="water, energy, climate change, etc..." class="rounded-xl w-full text-center block pl-10">
                 </div>
             </form>
 

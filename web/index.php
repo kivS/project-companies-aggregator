10a103bb7e030e437997b4237ac3b037a74b3a83
Vik Borges <v1k@protonmail.com>
2022-01-01 19:17:26 +0000

    replaced ring color

--- web/index.php
+++ web/index.php
@@ -208,7 +208,7 @@ if (isset($_GET['problem']) &&  strlen($_GET['problem']) > 1) {
                 <!-- Modal panel, show/hide based on modal state. -->
                 <div x-show="companyDetailModalShow" @keydown.window.escape="companyDetailModalShow = false" @click.outside="companyDetailModalShow = false" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-transition:enter-end="opacity-100 translate-y-0 sm:scale-10" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
                     <div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
-                        <button type="button" @click="companyDetailModalShow = false" class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <button type="button" @click="companyDetailModalShow = false" class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-500">
                             <span class="sr-only">Close</span>
                             <!-- Heroicon name: outline/x -->
                             <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -285,7 +285,7 @@ if (isset($_GET['problem']) &&  strlen($_GET['problem']) > 1) {
                     </div>
 
                     <div class="mt-5 sm:mt-4 sm:ml-10 sm:pl-4 sm:flex justify-end">
-                        <button type="button" @click="companyDetailModalShow = false" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 px-4 py-2 bg-white text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                        <button type="button" @click="companyDetailModalShow = false" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 px-4 py-2 bg-white text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
                             Close
                         </button>
                     </div>

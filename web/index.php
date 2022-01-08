baa829f8a0ace3e6581e6b4ab1645942219b856d
Vik Borges <v1k@protonmail.com>
2022-01-08 17:16:23 +0000

    fixed bug where jsonencode breaks on highlights data html encoding cuz of chars (space and shit)

--- web/index.php
+++ web/index.php
@@ -175,8 +175,11 @@ if (isset($_GET['problem']) &&  strlen($_GET['problem']) > 1) {
 
                                         $highlights[] = $tag;
                                     }
+
+                                    // encode html and clean it so we can usin it in the data- DOM
+                                    $highlights = htmlentities(json_encode($highlights))
                                     ?>
-                                    <button title="How I got this result?" data-highlights='<?= json_encode($highlights); ?>' @click="companyHighlights = JSON.parse($el.dataset.highlights); companyHighlightsModalShow = true;" class="absolute touch p-1  hover:cursor-help right-0">
+                                    <button title="How I got this result?" data-highlights='<?= $highlights; ?>' @click="companyHighlights = JSON.parse($el.dataset.highlights); companyHighlightsModalShow = true;" class="absolute touch p-1  hover:cursor-help right-0">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                             <path d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2h-1.528A6 6 0 004 9.528V4z" />
                                             <path fill-rule="evenodd" d="M8 10a4 4 0 00-3.446 6.032l-1.261 1.26a1 1 0 101.414 1.415l1.261-1.261A4 4 0 108 10zm-2 4a2 2 0 114 0 2 2 0 01-4 0z" clip-rule="evenodd" />

4d8181ca8a94a825b4a368a86a7f750d493fd0b6
Vik Borges <v1k@protonmail.com>
2022-01-08 16:04:34 +0000

    added sentry to template

--- .env.template.php
+++ .env.template.php
@@ -3,6 +3,8 @@ define('PROJECT_URL', '');
 
 define('USE_NINJA_ANALYTICS', false);
 
+define('SENTRY_DSN', '');
+
 define('MEILISEARCH_CLIENT_URL', 'http://127.0.0.1:7700');
 define('MEILISEARCH_APP_INDEX', '');
 define('MEILISEARCH_API_KEY', '');

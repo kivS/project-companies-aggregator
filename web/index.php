1ccbe1844b58eb68d86ccb3c744be593122f8a3a
Vik Borges <v1k@protonmail.com>
2022-01-17 16:37:29 +0000

    update phrazing

--- web/index.php
+++ web/index.php
@@ -404,7 +404,7 @@ if (isset($_GET['problem']) &&  strlen($_GET['problem']) > 1) {
                     <div class="sm:flex sm:items-start">
                         <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                             <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
-                                Search matches
+                                Matched on:
                             </h3>
 
                             <div class="mt-5">
